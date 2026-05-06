0d39b7aef806ff0369e140c5e056a82246e37b8d
Tej Desai <tej.desai@student.hhs.school.nz>
2026-05-07 09:35:05 +1200

    Deleting Files

--- enemy.py
+++
@@ -1,5 +0,0 @@
-import pygame
-class Enemy(pygame.sprite):
-    def __init__(self,x,y,w,h,"enemy")
-
-    def images():
--- python.classes.py
+++
@@ -1,6 +0,0 @@
-class MyClass:
-    x = 5
-print(MyClass)
-
-p1=MyClass()
-print(p1.x)
